ab61a9ba4df68d1c1eddd8171b281776b150d590
novol <groundforr@gmail.com>
2024-11-20 12:56:39 +0100

    Initial Commit

---
+++ NetworkManager/NetworkManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManagerApp.swift
+//  NetworkManager
+//
+//  Created by Влад Новолоакэ on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
